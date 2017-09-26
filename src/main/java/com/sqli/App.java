93f6d9824201de8a64333b76643584cbbea5e162
quentindebout <quentin.debout@cpe.fr>
2017-09-26 10:53:33 +0200

    First commit

---
+++ src/main/java/com/sqli/App.java
@@ -0,0 +1,19 @@
+package com.sqli;
+
+import org.joda.time.DateTime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        DateTime now =new DateTime();
+        
+        System.out.println(now);
+    }
+}
